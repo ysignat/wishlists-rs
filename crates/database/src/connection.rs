ec6d644c23cb3ff92de97904b52ca8e21004920b
Игнатов Егор Сергеевич <ysignat5@mts.ru>
2023-09-07 14:48:17 +0000

    .

--- crates/database/src/connection.rs
+++ crates/database/src/connection.rs
@@ -4,32 +4,59 @@ use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 use crate::DataError;
 
-pub struct Connection {
-    url: String,
-    acquire_timeout: u64,
-    max_connections: u32,
+pub struct DatabaseConnectOptions {
+    pub url: String,
+    pub max_connections: Option<u32>,
+    pub min_connections: Option<u32>,
+    pub connect_timeout: Option<Duration>,
+    pub idle_timeout: Option<Duration>,
+    pub acquire_timeout: Option<Duration>,
+    pub max_lifetime: Option<Duration>,
 }
 
-impl Connection {
-    #[must_use]
-    pub fn new(url: String, acquire_timeout: u64, max_connections: u32) -> Self {
-        Connection {
-            url,
-            acquire_timeout,
-            max_connections,
-        }
-    }
+impl From<DatabaseConnectOptions> for ConnectOptions {
+    fn from(value: DatabaseConnectOptions) -> Self {
+        let mut connection_opts = ConnectOptions::new(value.url);
 
+        if let Some(max_connections) = value.max_connections {
+            connection_opts.max_connections(max_connections);
+        }
+
+        if let Some(min_connections) = value.min_connections {
+            connection_opts.min_connections(min_connections);
+        }
+
+        if let Some(connect_timeout) = value.connect_timeout {
+            connection_opts.connect_timeout(connect_timeout);
+        }
+
+        if let Some(idle_timeout) = value.idle_timeout {
+            connection_opts.idle_timeout(idle_timeout);
+        }
+
+        if let Some(acquire_timeout) = value.acquire_timeout {
+            connection_opts.acquire_timeout(acquire_timeout);
+        }
+
+        if let Some(max_lifetime) = value.max_lifetime {
+            connection_opts.max_lifetime(max_lifetime);
+        }
+
+        connection_opts
+    }
+}
+
+pub struct Connection;
+
+impl Connection {
     /// # Errors
     ///
     /// Will return `DataError` if database, specified in URL, is unreachable
-    pub async fn connect(&self) -> Result<DatabaseConnection, DataError> {
-        let mut connection_opts = ConnectOptions::new(self.url.clone());
-        connection_opts
-            .acquire_timeout(Duration::from_secs(self.acquire_timeout))
-            .max_connections(self.max_connections);
-
-        Database::connect(connection_opts)
+    pub async fn connect(
+        database_connect_options: DatabaseConnectOptions,
+    ) -> Result<DatabaseConnection, DataError> {
+        let connect_options: ConnectOptions = database_connect_options.into();
+        Database::connect(connect_options)
             .await
             .or(Err(DataError::Unknown))
     }
--- src/config.rs
+++ src/config.rs
@@ -1,18 +1,70 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
 use clap::{Args, Parser, Subcommand};
+use database::connection::DatabaseConnectOptions;
 // use tracing_subscriber::filter::LevelFilter; // TODO: Preparation for logging
 
-const ENV_PREFIX: &str = "WISHLISTS__";
+const ENV_PREFIX: &str = "WISHLISTS";
+const ENV_SEPARATOR: &str = "__";
 
-const DATABASE_ENV_PREFIX: &str = "DATABASE__";
-const RUN_ENV_PREFIX: &str = "RUN__";
-// const LOG_ENV_PREFIX: &str = "LOG__"; // TODO: Preparation for logging
+const DATABASE_ENV_PREFIX: &str = "DATABASE";
+const RUN_ENV_PREFIX: &str = "RUN";
+// const LOG_ENV_PREFIX: &str = "LOG"; // TODO: Preparation for logging
 
-fn add_prefix(values: &[&str]) -> String {
-    let concatenated = values.concat();
-    format!("{ENV_PREFIX}{concatenated}")
+const LONG_SEPARATOR: &str = "-";
+const DATABASE_LONG_PREFIX: &str = "database";
+const RUN_LONG_PREFIX: &str = "run";
+// const LOG_LONG_PREFIX: &str = "log"; // TODO: Preparation for logging
+
+struct ArgMetadata {
+    prefix: Option<String>,
+    separator: String,
 }
+
+trait Metadata {
+    fn metadata() -> ArgMetadata;
+}
+
+struct EnvArg;
+
+impl Metadata for EnvArg {
+    fn metadata() -> ArgMetadata {
+        ArgMetadata {
+            prefix: Some(ENV_PREFIX.to_owned()),
+            separator: ENV_SEPARATOR.to_owned(),
+        }
+    }
+}
+
+struct LongArg;
+
+impl Metadata for LongArg {
+    fn metadata() -> ArgMetadata {
+        ArgMetadata {
+            prefix: None,
+            separator: LONG_SEPARATOR.to_owned(),
+        }
+    }
+}
+
+trait Construct
+where
+    Self: Metadata,
+{
+    fn construct(values: &[&str]) -> String {
+        let mut res = String::new();
+        let metadata = Self::metadata();
+        if let Some(prefix) = metadata.prefix {
+            res.push_str(&prefix);
+            res.push_str(&metadata.separator);
+        };
+        res.push_str(&values.join(&metadata.separator));
+        res
+    }
+}
+
+impl Construct for EnvArg {}
+impl Construct for LongArg {}
 
 #[derive(Parser)]
 pub struct Config {
@@ -35,29 +87,69 @@ pub enum Commands {
 #[derive(Args)]
 pub struct DatabaseArgs {
     #[arg(
-        long = "database-url",
-        env = add_prefix(&[DATABASE_ENV_PREFIX,"URL"]),
+        long = LongArg::construct(&[DATABASE_LONG_PREFIX,"url"]),
+        env = EnvArg::construct(&[DATABASE_ENV_PREFIX,"URL"]),
         default_value = "postgres://postgres:postgres@postgres:5432/postgres",
-        help = "URL for database connection (postgres://<user>:<password>@<host>:<port>/<database>)",
+        help = "The URI of the database",
         global = true
     )]
-    pub url: String,
+    url: String,
     #[arg(
-        long = "database-pool-size",
-        env = add_prefix(&[DATABASE_ENV_PREFIX,"SIZE"]),
-        default_value = "5",
-        help = "Connection pool size",
+        long = LongArg::construct(&[DATABASE_LONG_PREFIX,"max-connections"]),
+        env = EnvArg::construct(&[DATABASE_ENV_PREFIX,"MAX_CONNECTIONS"]),
+        help = "Maximum number of connections for a pool",
         global = true
     )]
-    pub pool_size: u32,
+    max_connections: Option<u32>,
     #[arg(
-        long = "database-acquire-timeout",
-        env = add_prefix(&[DATABASE_ENV_PREFIX,"ACQUIRE_TIMEOUT"]),
-        default_value = "3",
-        help = "TODO: write description",
+        long = LongArg::construct(&[DATABASE_LONG_PREFIX,"min-connections"]),
+        env = EnvArg::construct(&[DATABASE_ENV_PREFIX,"MIN_CONNECTIONS"]),
+        help = "Minimum number of connections for a pool",
         global = true
     )]
-    pub acquire_timeout: u64,
+    min_connections: Option<u32>,
+    #[arg(
+        long = LongArg::construct(&[DATABASE_LONG_PREFIX,"connect-timeout"]),
+        env = EnvArg::construct(&[DATABASE_ENV_PREFIX,"CONNECT_TIMEOUT"]),
+        help = "The connection timeout for a packet connection",
+        global = true
+    )]
+    connect_timeout: Option<u64>,
+    #[arg(
+        long = LongArg::construct(&[DATABASE_LONG_PREFIX,"idle-timeout"]),
+        env = EnvArg::construct(&[DATABASE_ENV_PREFIX,"IDLE_TIMEOUT"]),
+        help = "Maximum idle time for a particular connection to prevent network resource exhaustion",
+        global = true
+    )]
+    idle_timeout: Option<u64>,
+    #[arg(
+        long = LongArg::construct(&[DATABASE_LONG_PREFIX,"acquire-timeout"]),
+        env = EnvArg::construct(&[DATABASE_ENV_PREFIX,"ACQUIRE_TIMEOUT"]),
+        help = "Maximum amount of time to spend waiting for acquiring a connection",
+        global = true
+    )]
+    acquire_timeout: Option<u64>,
+    #[arg(
+        long = LongArg::construct(&[DATABASE_LONG_PREFIX,"max-lifetime"]),
+        env = EnvArg::construct(&[DATABASE_ENV_PREFIX,"MAX_LIFETIME"]),
+        help = "Maximum lifetime of individual connections",
+        global = true
+    )]
+    max_lifetime: Option<u64>,
+}
+
+impl From<DatabaseArgs> for DatabaseConnectOptions {
+    fn from(value: DatabaseArgs) -> Self {
+        DatabaseConnectOptions {
+            url: value.url,
+            max_connections: value.max_connections,
+            min_connections: value.min_connections,
+            connect_timeout: value.connect_timeout.map(Duration::from_secs),
+            idle_timeout: value.idle_timeout.map(Duration::from_secs),
+            acquire_timeout: value.acquire_timeout.map(Duration::from_secs),
+            max_lifetime: value.max_lifetime.map(Duration::from_secs),
+        }
+    }
 }
 
 // #[derive(Args)] // TODO: Preparation for logging
@@ -77,15 +169,15 @@ pub struct DatabaseArgs {
 #[derive(Args, PartialEq, Eq)]
 pub struct RunArgs {
     #[arg(
-        long = "run-root-path",
-        env = add_prefix(&[RUN_ENV_PREFIX,"ROOT_PATH"]),
+        long = LongArg::construct(&[RUN_LONG_PREFIX,"root-path"]),
+        env = EnvArg::construct(&[RUN_ENV_PREFIX,"ROOT_PATH"]),
         default_value = "/api",
         help = "Root path for deployment under reverse proxy (must start with '/')"
     )]
     pub root_path: RootPath,
     #[arg(
-        long = "run-bind-address",
-        env = add_prefix(&[RUN_ENV_PREFIX,"BIND_ADDRESS"]),
+        long = LongArg::construct(&[RUN_LONG_PREFIX,"bind-address"]),
+        env = EnvArg::construct(&[RUN_ENV_PREFIX,"BIND_ADDRESS"]),
         default_value = "127.0.0.1:8080",
         help = "Address where app listens to incoming connections (<host>:<port>)"
     )]
@@ -99,8 +191,12 @@ pub struct RootPath {
 
 impl From<String> for RootPath {
     fn from(value: String) -> Self {
-        RootPath {
-            root_path: if value == "/" { String::new() } else { value },
+        if value.starts_with('/') {
+            RootPath {
+                root_path: if value == "/" { String::new() } else { value },
+            }
+        } else {
+            panic!("Root path '{value}' doesn't start with '/'")
         }
     }
 }
